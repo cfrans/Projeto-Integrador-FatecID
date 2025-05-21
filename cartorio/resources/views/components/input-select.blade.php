2b8d5397f53ec94c668634ffc6f1bad375376136
Vitoria Faturi <cvitoriak@gmail.com>
2025-05-20 21:57:55 -0300

    Início da estilização da página de protocolos

---
+++ cartorio/resources/views/components/input-select.blade.php
@@ -0,0 +1,20 @@
+@props(['disabled' => false])
+
+<select
+    @disabled($disabled)
+    {{ $attributes->merge([
+        'class' => '
+            border-0
+            bg-[#f5f5f5]
+            dark:text-black-300
+            focus:border-0
+            focus:ring-0
+            rounded-md
+            shadow-sm
+        '
+    ]) }}
+>
+    {{ $slot }}
+</select>
+
+
