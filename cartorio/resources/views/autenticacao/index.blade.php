655de7b5b530120b2ff0f2907ecfba8f32927dd0
Pedro <pedrodeoliveira.pos@gmail.com>
2025-05-27 21:18:15 -0300

    Ajustado tamanho dos campos do formulario

--- cartorio/resources/views/autenticacao/index.blade.php
+++ cartorio/resources/views/autenticacao/index.blade.php
@@ -24,14 +24,14 @@
 
             <div class="flex flex-wrap justify-start w-[92%] bg-white rounded-t-md p-4">
 
-                <div class ="flex flex-wrap justify-start w-full rounded-t-md p-4">
+                <div class ="flex flex-wrap justify-start w-full">
                     <!-- Primeira coluna (1/8) -->
                         <div class="campo-formulario flex items-center ml-6"> 
                             <div class="text-left">
                                 <x-input-label for="autenticacao_data">
                                     Data
                                 </x-input-label>
-                                <x-input-naoalteravel id="autenticacao_data" name="autenticacao_data" class="w-[200px] h-7.5 text-sm">
+                                <x-input-naoalteravel id="autenticacao_data" name="autenticacao_data" class="w-[200px] h-10 text-sm">
                                 </x-input-naoalteravel>
                             </div>
                         </div>
@@ -42,7 +42,7 @@
                                 <x-input-label for="autenticacao_hora">
                                     Hora
                                 </x-input-label>
-                                <x-input-naoalteravel id="autenticacao_hora" name="autenticacao_hora" class="w-[200px] h-7.5 text-sm">
+                                <x-input-naoalteravel id="autenticacao_hora" name="autenticacao_hora" class="w-[200px] h-10 text-sm">
                                 </x-input-naoalteravel>
                             </div>
                         </div>  
@@ -53,19 +53,19 @@
                                 <x-input-label for="autenticacao_usuario">
                                     Usúario
                                 </x-input-label>
-                                <x-input-naoalteravel id="nome" name="nome" class="mt-1 block w-full" value="{{ Auth::user() ? Auth::user()->nome : '' }}" readonly />
+                                <x-input-naoalteravel id="nome" name="nome" class="w-[200px] h-10 text-sm" value="{{ Auth::user() ? Auth::user()->nome : '' }}" readonly />
                             </div>
                         </div>
                 </div>
 
-                <div class ="flex flex-wrap justify-start w-full rounded-t-md p-4">
+                <div class ="flex flex-wrap justify-start w-full">
                         <!-- Quarta coluna (4/8) -->
-                        <div class="campo-formulario flex items-center ml-6 w-[200px]"> 
+                        <div class="campo-formulario flex items-center ml-6"> 
                             <div class="text-left">
                                 <x-input-label for="autenticacao_grupo">
                                     Grupo
                                 </x-input-label>
-                                <x-input-naoalteravel id="protocolo_natureza" name="protocolo_natureza" class="w-[200px] h-7.5 text-sm">
+                                <x-input-naoalteravel id="protocolo_natureza" name="protocolo_natureza" class="w-[435px] h-10 text-sm">
                                 </x-input-naoalteravel>
                             </div>
                         </div>
@@ -76,44 +76,44 @@
                                 <x-input-label for="autenticacao_protocolo">
                                     Protocolo
                                 </x-input-label>
-                                <x-input-naoalteravel id="autenticacao_protocolo" name="autenticacao_protocolo" class="w-[200px] h-7.5 text-sm">
+                                <x-input-naoalteravel id="autenticacao_protocolo" name="autenticacao_protocolo" class="w-[200px] h-10 text-sm">
                                 </x-input-naoalteravel>
                             </div>
                         </div>
                 </div>
 
-                <div class ="flex flex-wrap justify-start w-full rounded-t-md p-4">
+                <div class ="flex flex-wrap justify-start w-full">
                         <!-- Quinta coluna (5/8) -->
                                 <div class="campo-formulario flex items-center ml-6"> 
                                         <div class="text-left">
                                             <x-input-label for="autenticacao_apresentante">
                                                 Apresentante
                                             </x-input-label>
-                                            <x-input-naoalteravel id="apresentante_nome" name="apresentante_nome" class="w-[200px] h-7.5 text-sm">
+                                            <x-input-naoalteravel id="apresentante_nome" name="apresentante_nome" class="w-[435px] h-10 text-sm">
                                             </x-input-naoalteravel>
                                         </div>
                                     </div>
                     
                         <!-- Sexta coluna (6/8) -->
-                        <div class="campo-formulario flex items-center"> 
+                        <div class="campo-formulario flex items-center ml-6"> 
                             <div class="text-left">
                                 <x-input-label for="autenticacao_valor">
                                     Valor
                                 </x-input-label>
-                                    <x-input-number type="text" id="valor" name="valor" class="w-[200px] h-8 text-sm" required>
+                                    <x-input-number type="text" id="valor" name="valor" class="w-[200px] h-10 text-sm" required>
                                     </x-input-number>
                             </div>
                         </div>
                     </div>
 
-                    <div class ="flex flex-wrap justify-start w-full rounded-t-md p-4">
+                    <div class ="flex flex-wrap justify-start w-full">
                             <!-- Sétima coluna (7/8) -->
                             <div class="campo-formulario flex items-center ml-6"> 
                                 <div class="text-left">
                                     <x-input-label for="autenticacao_pagamento">
                                         Forma de Pagamento
                                     </x-input-label>
-                                    <x-input-select id="id_forma_pagamento" name="id_forma_pagamento" class="w-[200px] h-8 text-sm" required>
+                                    <x-input-select id="id_forma_pagamento" name="id_forma_pagamento" class="w-[340px] h-10 text-sm" required>
                                         <option value="1">Dinheiro</option>
                                         <option value="2">Pix</option>
                                         <option value="3">Ted</option>
@@ -123,12 +123,12 @@
                             </div>
 
                             <!-- Oitava coluna (8/8) -->
-                            <div class="campo-formulario flex items-center"> 
+                            <div class="campo-formulario flex items-center ml-6"> 
                                 <div class="text-left">
                                     <x-input-label for="autenticacao_banco">
                                         Banco
                                     </x-input-label>
-                                    <x-input-select id="id_banco" name="id_banco" class="w-[200px] h-8 text-sm" required>
+                                    <x-input-select id="id_banco" name="id_banco" class="w-[300px] h-10 text-sm" required>
                                         <option value="1">Caixa Econômica</option>
                                         <option value="2">Banco do Brasil</option>
                                         <option value="3">Itaú</option>
@@ -139,34 +139,34 @@
                             </div>
                     </div>
 
-                    <div class ="flex flex-wrap justify-start w-full rounded-t-md p-4">
+                    <div class ="flex flex-wrap justify-start w-full">
                             <!-- Primeira coluna (1/3) -->
                             <div class="campo-formulario flex items-center ml-6"> 
                                 <div class="text-left">
                                     <x-input-label for="autenticacao_conta">
                                         Conta
                                     </x-input-label>
-                                    <x-text-input id="autenticacao_conta" name="autenticacao_conta" class="w-[200px] h-8 text-sm" required />
+                                    <x-text-input id="autenticacao_conta" name="autenticacao_conta" class="w-[200px] h-10 text-sm" required />
                                 </div>
                             </div>
 
                             <!-- Segunda coluna (1/2) -->
-                            <div class="campo-formulario flex items-center"> 
+                            <div class="campo-formulario flex items-center ml-6"> 
                                 <div class="text-left">
                                     <x-input-label for="autenticacao_agencia">
                                         Agência
                                     </x-input-label>
-                                    <x-text-input id="autenticacao_agencia" name="autenticacao_agencia" class="w-[200px] h-8 text-sm" required />
+                                    <x-text-input id="autenticacao_agencia" name="autenticacao_agencia" class="w-[200px] h-10 text-sm" required />
                                 </div>
                             </div>
 
                             <!-- Terceira coluna (3/2) -->
-                            <div class="campo-formulario flex items-center"> 
+                            <div class="campo-formulario flex items-center ml-6"> 
                                 <div class="text-left">
                                     <x-input-label for="autenticacao_cheque">
                                         Número do cheque
                                     </x-input-label>
-                                    <x-text-input id="autenticacao_cheque" name="autenticacao_cheque" class="w-[200px] h-8 text-sm" required />
+                                    <x-text-input id="autenticacao_cheque" name="autenticacao_cheque" class="w-[200px] h-10 text-sm" required />
                                 </div>
                             </div>
                     </div>
